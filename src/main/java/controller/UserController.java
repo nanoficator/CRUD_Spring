c741891e886b86e07ff57cfe9a1be8a96afd94ad
Mikhail Kuzivanov <mkuzivanov@gmail.com>
2020-01-21 18:58:43 +0300

    Spring

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView allUsers() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("tablePage");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/admin/add")
+    public ModelAndView addUser() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("addPage");
+        return modelAndView;
+    }
+}
