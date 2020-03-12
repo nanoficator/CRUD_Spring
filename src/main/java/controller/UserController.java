cc2a61e491d306f1d16d3ee95dcd02fb8b81c2ee
Mikhail Kuzivanov <mkuzivanov@gmail.com>
2020-03-12 18:18:06 +0300

    CRUD_Spring

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import service.RoleService;
 import service.UserService;
 import org.springframework.stereotype.Controller;
